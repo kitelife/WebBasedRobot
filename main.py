5bf6f62149c4b1226df2527138dccdc64d328a54
xiayf <sas.198708@gmail.com>
2012-02-13 22:36:35 +0800

    modify main.py and add index.html

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-# web-based swarm-robots remote control system,
-# the server end is based on tornado(www.tornadoweb.org)
-# 
-# author: xiayf
-# email: youngsterxyf@gmail.com
-
 import tornado.ioloop
 import tornado.web
 
-class MainHandler(tornado.web.RequestHandler):
+class mainHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.write("Hello,World")
+		self.render("index.html", title="formpost")
+	
+	def post(self):
+		#print self.request.arguments
+		#if 'firstname' in self.request.arguments.keys():
+		#	print self.get_argument("firstname")
+		#if 'lastname' in self.request.arguments.keys():
+		#	print self.get_argument("lastname")
+		if 'cmd' in self.request.arguments.keys():
+			print self.get_argument("cmd")
+		self.render("index.html", title="formpost")
 
 application = tornado.web.Application([
-	(r"/", MainHandler),
+	(r"/", mainHandler),
 ])
 
-if __name__ == '__main__':
+if __name__=="__main__":
 	application.listen(8888)
 	tornado.ioloop.IOLoop.instance().start()
