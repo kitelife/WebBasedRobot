d0b46091efe8c8235b4e7da0e814753e111f9d08
youngsterxyf <youngsterxiayf@gmail.com>
2013-01-06 02:07:12 +0800

    implement version number of command, automatic login IP camera website

--- server_code/operate_serial.py
+++ server_code/operate_serial.py
@@ -9,9 +9,10 @@ CMD_CODE = {
     'turnleft': '5',
     'turnright': '6',
     'stop': '7',
-    'count': '9'
+    'count': '8'
 }
 
+version_dict = {'1': 0, '2': 0, '3': 0, '4': 0, '5': 0}
 
 def scan():
     '''scan for available ports. return a list of tuples (num, name)'''
@@ -25,27 +26,15 @@ def scan():
             pass
     return available
 
-'''
-class user_serial(object):
-    
-    def __init__(self, baudrate=9600, port=1):
-        self.ser = serial.Serial()
-        self.ser.setBaudrate(baudrate)
-        self.ser.setPort(port)
-
-    def send_data(self, data):
-        self.ser.write(data)
-
-    def close(self):
-        self.ser.close()
-'''
-
 def send_cmd(cmd, target):
+
+    encoded_cmd_list = list()
+
     '''
     cmd: 包含两个部分---指令本身以及其参数，以空格分隔
     target: 小车的编号
     '''
-    result_value = 'true'
+    result_value = ''
     
     data_list = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', 
                     '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
@@ -54,31 +43,63 @@ def send_cmd(cmd, target):
 
     if len(cmd_list) and (cmd_list[0] in CMD_CODE.keys()):
         cmd_code = CMD_CODE[cmd_list[0]]
-        '''指令编码使用1,2两个字节存储，1字节存编码的十位，2字节存编码的个位'''
+
+        '''指令编码使用2,3两个字节存储，2字节存编码的十位，3字节存编码的个位'''
         if len(cmd_code) == 1:
-            data_list[2] = cmd_code
+            data_list[3] = cmd_code
         elif len(cmd_code) == 2:
-            data_list[1] = cmd_code[0]
-            data_list[2] = cmd_code[1]
+            data_list[2] = cmd_code[0]
+            data_list[3] = cmd_code[1]
 
     if len(cmd_list) == 2:
         cmd_arg = cmd_list[1].strip()
-        '''指令参数编码使用3，4两个字节存储，3字节存储编码的十位，4字节存编码的个位'''
+        '''指令参数编码使用4，5两个字节存储，4字节存储编码的十位，5字节存编码的个位'''
         if len(cmd_arg) == 1:
-            data_list[4] = cmd_arg
+            data_list[5] = cmd_arg
         elif len(cmd_arg) == 2:
-            data_list[3] = cmd_arg[0]
-            data_list[4] = cmd_arg[1]
+            data_list[4] = cmd_arg[0]
+            data_list[5] = cmd_arg[1]
     '''使用"x"来指代所有小车'''
     if target == 'all':
         target_code = 'x'
     else:
         target_code = target
-    '''32字节数据的0字节存储小车编号'''
-    data_list[0] = target_code
 
-    data_str = ('').join(data_list)
-    print '%s' % data_str
+    ''' 更新版本号
+        设置小车编号
+        设置版本号
+    '''
+    if target_code == 'x':
+        for key in version_dict.keys():
+            version_dict[key] += 1
+            one_target_data_list = data_list
+            one_target_data_list[1] = key
+            version = version_dict[key]
+            if version >= 10000:
+                version = 1
+            index = 8
+            while version > 0:
+                remainder = version % 10
+                one_target_data_list[index] = str(remainder)
+                version = version / 10
+                index -= 1
+            one_target_data_str = ''.join(one_target_data_list)
+            encoded_cmd_list.append(one_target_data_str)
+    else:
+        version_dict[target_code] = version_dict.get(target_code, 0) + 1
+        data_list[1] = target_code
+        version = version_dict[target_code]
+        if version >= 10000:
+            version = 1
+        index = 8
+        while version > 0:
+            remainder = version % 10
+            data_list[index] = str(remainder)
+            version = version / 10
+            index -= 1
+        data_str = ''.join(data_list)
+        encoded_cmd_list.append(data_str)        
+    print encoded_cmd_list
     try:
         serial_handler = serial.Serial(2,
                                         baudrate=345600,
@@ -93,24 +114,25 @@ def send_cmd(cmd, target):
         serial_handler.setDTR(1)
         serial_handler.flushInput()
         serial_handler.flushOutput()
-        serial_handler.write(data_str)
-        if cmd_list[0] == "info":
-            response = serial_handler.read(32)
-            print response
-            result_value = response[0] + "号小车 --- " + "电量：" + response[1] + "." + response[2] + "(伏)；"
-            result_value += "左电机: " + response[3] + response[4] + response[5] + response[6] + "，"
-            result_value += "右电机：" + response[7] + response[8] + response[9] + response[10]
-
-            print result_value
+        for encoded_cmd in encoded_cmd_list:
+            serial_handler.write(data_str)
+            if cmd_list[0] == "info":
+                response = serial_handler.read(32)
+                print response
+                result_value += response[0] + "号小车 --- " + "电量：" + response[1] + "." + response[2] + "(伏)；"
+                result_value += "左电机: " + response[3] + response[4] + response[5] + response[6] + "，"
+                result_value += "右电机：" + response[7] + response[8] + response[9] + response[10]
     except Exception, e:
         result_value = e.message.decode('gbk').encode('utf-8')
-
+    if result_value == '':
+        result_value = 'true'
     return result_value
 
 
 if __name__ == '__main__':
 
     import time
+    
     send_cmd('info', 'all')
     time.sleep(1)
     send_cmd('run', '1')
